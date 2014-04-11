067416a9fd5f0d9b65b8dceef931955d7de949cc
Ana-Maria Vladu <vladu.anamaria@yahoo.com>
2014-04-12 00:14:28 +0300

    The rest of the commands

--- voiceRec.js
+++ voiceRec.js
@@ -289,6 +289,82 @@ window.onload = function (e) {
 			}
 			is_news_feed=0;
 			break;
+
+		command: "Privacy"
+			});
+			if (is_settings==1){
+			location.href = "https://www.facebook.com/settings?tab=privacy"
+			}
+			is_news_feed=0;
+			break;	
+		command: "Timeline"
+			});
+			if (is_settings==1){
+			location.href = "https://www.facebook.com/settings?tab=timeline"
+			}
+			is_news_feed=0;
+			break;	
+		command: "Blocking"
+			});
+			if (is_settings==1){
+			location.href = "https://www.facebook.com/settings?tab=blocking"
+			}
+			is_news_feed=0;
+			break;	
+		command: "Notifications"
+			});
+			if (is_settings==1){
+			location.href = "https://www.facebook.com/settings?tab=notifications"
+			}
+			is_news_feed=0;
+			break;	
+		command: "Mobile"
+			});
+			if (is_settings==1){
+			location.href = "https://www.facebook.com/settings?tab=mobile"
+			}
+			is_news_feed=0;
+			break;	
+
+		command: "Followers"
+			});
+			if (is_settings==1){
+			location.href = "https://www.facebook.com/settings?tab=followers"
+			}
+			is_news_feed=0;
+			break;	
+		command: "Applications"
+			});
+			if (is_settings==1){
+			location.href = "https://www.facebook.com/settings?tab=applications"
+			}
+			is_news_feed=0;
+			break;	
+		command: "Ads"
+			});
+			if (is_settings==1){
+			location.href = "https://www.facebook.com/settings?tab=ads"
+			}
+			is_news_feed=0;
+			break;
+		command: "Payments"
+			});
+			if (is_settings==1){
+			location.href = "https://secure.facebook.com/settings?tab=payments"
+			}
+			is_news_feed=0;
+			break;
+		command: "Support"
+			});
+			if (is_settings==1){
+			location.href = "https://www.facebook.com/settings?tab=support"
+			}
+			is_news_feed=0;
+			break;
+	
+
+
+		
                     default:
                         port.postMessage({
                             command: "Sorry, I didn't get that."
